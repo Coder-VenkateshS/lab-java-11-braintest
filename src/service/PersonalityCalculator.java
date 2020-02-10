9a31453677b681de278386cbdd5a55e1dbbd3c92
Teacher-Venkatesh <12venkat21@gmail.com>
2020-02-10 12:20:58 +0530

    test cases added

--- src/service/PersonalityCalculator.java
+++ src/service/PersonalityCalculator.java
@@ -29,14 +29,13 @@ B = __ + __ + __ + __ + __ + __ + __ + __ + __ = _____
 */
 	
 	
-	
 	public String findYourBrainType(String options) {
 		int[] answers=findPersonality(options);
 		int q=5;
 		int A=0,B=0;
-		A=answers[0]+answers[1]+answers[2]+answers[5]+answers[7]+answers[9]+answers[10]+answers[11]+answers[13]+answers[17]+answers[19];
-		B=answers[3]+answers[5]+answers[6]+answers[9]+answers[12]+answers[14]+answers[15]+answers[16]+answers[18];
-		int braintype=66-(A+B);
+		A=answers[0]+answers[1]+answers[2]+answers[4]+answers[7]+answers[9]+answers[10]+answers[11]+answers[13]+answers[17]+answers[19];
+		B=answers[3]+answers[5]+answers[6]+answers[8]+answers[12]+answers[14]+answers[15]+answers[16]+answers[18];
+		int braintype=66-A+B;
 		if(braintype>=20 && braintype<=55)
 		{
 			return "leftbrained";
@@ -51,6 +50,8 @@ B = __ + __ + __ + __ + __ + __ + __ + __ + __ = _____
 		}
 		
 	}
+		
+	}
 
 	
 	
@@ -58,6 +59,4 @@ B = __ + __ + __ + __ + __ + __ + __ + __ + __ = _____
 	
 	
 	
-	
-	
-}
+
